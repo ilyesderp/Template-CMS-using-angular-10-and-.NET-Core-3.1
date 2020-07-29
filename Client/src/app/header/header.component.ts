f1db824249575762d20f35fc0078cf0fb90fc812
ilyes amerouali <ilyesamerouali@gmail.com>
2020-07-29 16:59:37 +0100

    Start of produits et services

--- Client/src/app/header/header.component.ts
+++ Client/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { faSignInAlt, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
 import { HeaderService } from '../shared/navbar.service';
 import { DataSotrageService } from '../shared/data-storage.service';
@@ -8,7 +8,7 @@ import { DataSotrageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   
   signInIcon = faSignInAlt;
   phoneIcon = faPhoneAlt;
@@ -17,26 +17,79 @@ export class HeaderComponent implements OnInit {
   selectedCountryCode2 = 'dz';
   //countryCodes = ['eu', 'us', 'jp', 'ch', 'se', 'dk', 'no', 'dz'];
   countryCodes = [];
-  unit: number;
-  shortName: string;
-  sellRate: number;
+  unit1: number;
+  shortName1: string;
+  sellRate1: number;
+  specialCase: string = '';
+
+  /*unit2: number;
+  shortName2: string;
+  sellRate2: number;*/
 
   taux: {unit: number, shortName: string, sellRate: number}[] = [];
 
   constructor(public headerService: HeaderService, private dataStorageService: DataSotrageService) { }
+  
 
   ngOnInit(): void {
     this.getTauxChange();
+    
+  }
+
+
+  ngAfterViewInit(): void {
+    /*this.changeSelectedCountryCodeOne("eu");
+    this.changeSelectedCountryCodeTwo("dz");*/
   }
 
 
   changeSelectedCountryCodeOne(value: string): void {
     this.selectedCountryCode1 = value;
+    
+    for (const elt of this.taux) {
+      let code: string = elt.shortName.substring(0,2);
+      code = code.toLowerCase();
+      if((code == 'cn') && (elt.shortName == "CNY") && (code == this.selectedCountryCode1) && (this.specialCase == 'CNH' || this.specialCase == '')){
+        this.unit1 = elt.unit;
+        this.shortName1 = elt.shortName;
+        this.sellRate1 = elt.sellRate;
+        this.specialCase = 'CNY'
+        break;
+      }
+      else if((code == 'cn') && (elt.shortName == "CNH") && (code == this.selectedCountryCode1) && (this.specialCase == 'CNY' || this.specialCase == '')){
+        this.unit1 = elt.unit;
+        this.shortName1 = elt.shortName;
+        this.sellRate1 = elt.sellRate;
+        this.specialCase = 'CNH'
+        break;
+      }
+      else if(code == this.selectedCountryCode1 && elt.shortName != "CNY" && elt.shortName != "CNH"){
+        this.unit1 = elt.unit;
+        this.shortName1 = elt.shortName;
+        this.sellRate1 = elt.sellRate;
+        break;
+      }
+
+    }
+
   }
 
-  //not used yet
+  
   changeSelectedCountryCodeTwo(value: string): void {
     this.selectedCountryCode2 = value;
+
+    /*for (const elt of this.taux) {
+      let code: string = elt.shortName.substring(0,2);
+      code = code.toLowerCase();
+      if(code == this.selectedCountryCode2){
+        this.unit2 = elt.unit;
+        this.shortName2 = elt.shortName;
+        this.sellRate2 = elt.sellRate;
+      }
+      else{
+        console.log("erreur countryCode");
+      }
+    }*/
   }
 
 
@@ -52,18 +105,10 @@ getTauxChange(){
         countryCode = countryCode.toLowerCase();
         this.countryCodes.push(countryCode);
 
-        if(this.selectedCountryCode1 == countryCode){
-          this.unit = elt.unit;
-          this.shortName = elt.shortName;
-          this.sellRate = elt.sellRate;
-        }
-        else{
-          console.log("erreur countryCode");
-        }
-
-        //this.taux.push({unit: elt.unit, shortName: elt.shortName, sellRate: elt.sellRate});
+        this.taux.push({unit: elt.unit, shortName: elt.shortName, sellRate: elt.sellRate});
         
       }
+      this.changeSelectedCountryCodeOne("eu");
   });
 }
 
